8225a1569874cefc28261488d26aa0a602305650
noriimi <m.wladzinski20@gmail.com>
2020-07-30 00:04:30 +0200

    Added new type sample_t, added logging to file for debug purposes, fixed behavior of last LED

--- GloS/thrFunc.h
+++ GloS/thrFunc.h
@@ -2,6 +2,7 @@
 #define NOMINMAX
 #define NUMOFLEDS 60
 #define NUMOFSAMPLES 512
+
 #include<iostream>
 #include<array>
 #include"PortsFinder.hpp"
@@ -11,14 +12,26 @@
 #include"RtAudio/RtAudio.h"
 #include"simple_fft/fft.hpp"
 #include"threadHelper.hpp"
-void thrFunc(int16_t data[],unsigned rs) noexcept//Placeholder function
+#define MULTIPLIER 1
+#define LOGf 0
+#if LOGf 
+#include<fstream>
+#endif
+void thrFunc(sample_t data[],unsigned rs) noexcept//Placeholder function
 {
+#if LOGf
+	std::ofstream myfile;
+	myfile.open("LOG.txt");
+	myfile << "Header\n";
+#endif
 	static std::array<double, NUMOFSAMPLES> A;
 	static std::array<complex_type, NUMOFSAMPLES> B;
 	static const char* error;
 	static unsigned cursor = 0;
 	static unsigned pos = 0;
+	static double maxo = 0;
 	static std::vector<double> pool;
+	static double tes;
 	uint8_t frame[184];
 	frame[0] = 0xDE;
 	frame[1] = 0xAD;
@@ -47,9 +60,7 @@ void thrFunc(int16_t data[],unsigned rs) noexcept//Placeholder function
 				int i = 0;
 				for (auto& it : A)
 				{
-					it = (data[i] / 32768.0f);
-					//it = (data[i] / 8192.0f);
-					//it=(0);
+					it = (data[i]);
 					i++;
 				}
 				threadHelper::staticFlags::s_read_ = false;
@@ -57,32 +68,49 @@ void thrFunc(int16_t data[],unsigned rs) noexcept//Placeholder function
 			simple_fft::FFT(A, B, NUMOFSAMPLES, error);
 			for (int i = 0; i < NUMOFSAMPLES/2; i++)
 			{
-				B.at(i).real(4 * std::abs(B.at(i)));
+				B.at(i).real( MULTIPLIER*std::abs(B.at(i))/256/4);
 			}
 			cursor = 0;
 			pos = 0;
+			maxo = (*std::max_element(B.begin(), B.begin()+255, [](const complex_type& lhs, const complex_type& rhs){ return rhs.real() > lhs.real(); })).real();
+			printf("%f\t", maxo);
+			tes = maxo;
+			maxo = 254 / maxo;
+			maxo = maxo >= 32 ? 32 : maxo;
+			maxo = maxo < 1 ? 1 : maxo;
 			for (float i = 0; i < NUMOFLEDS; i++)
 			{
 				
 				pos = std::ceil((i + 1.0f) / rs);
 				
 				frame[5 + 3 * (int)i] = 200;
-				for (int i = 1; i <= pos; i++)
+				for (int j = 1; j <= pos; j++)
 				{
-					pool.push_back(B.at(++cursor).real());
+					pool.push_back(B.at(cursor++).real());
 				}
-				frame[6 + 3 * (int)i] = (uint8_t)*std::max_element(pool.begin(), pool.end());
-				frame[4 + 3 * (int)i] = frame[6 + 3 * (int)i];
+				frame[4 + 3 * (int)i] = (uint8_t)(std::min(255.0,*std::max_element(pool.begin(), pool.end())));
+				frame[6 + 3 * (int)i] =frame[4 + 3 * (int)i]<20?0: frame[4 + 3 * (int)i];
+#if LOGf
+				myfile << (int)frame[6 + 3 * (int)i] << '\t';
+#endif
 				pool.clear();  
 
 			}
 
 
-
 			
 			serial.UpdateLED(frame, 184);
+			printf("%f\t%f\n", maxo,maxo*tes);
+#if LOGf
+			myfile << '\n';
+#endif
 		}
 		if (threadHelper::staticFlags::s_kill_)
+		{
+#if LOGf 
+			myfile.close();
+#endif
 			return;
+		}
 	}
 }
